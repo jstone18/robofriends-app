122b9f023ee8067b83e0f7774d0a1d969bda7e1d
jstone18 <jason.stone0808@gmail.com>
2019-12-26 22:48:01 -0500

    Update app file structure. Refactor App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import "./App.css";
-import Scroll from "./Scroll";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import "../css/App.css";
+import Scroll from "../components/Scroll";
 
 class App extends Component {
 	constructor() {
@@ -25,19 +25,15 @@ class App extends Component {
 	};
 
 	render() {
-		const filteredRobots = this.state.robots.filter(robot => {
-			return (
-				robot.name
-					.toLowerCase()
-					.indexOf(this.state.searchfield.toLowerCase()) !== -1
-			);
+		const { robots, searchfield } = this.state;
+
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().indexOf(searchfield.toLowerCase()) !== -1;
 		});
 
-		if (this.state.robots.length === 0) {
-			return <h1>Loading...</h1>;
-		}
-
-		return (
+		return !robots ? (
+			<h1>Loading...</h1>
+		) : (
 			<div className="tc">
 				<h1 className="f1">RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange} />
