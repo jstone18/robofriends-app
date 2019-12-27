16fda0227b764722e2995dc42737bb27878b3309
jstone18 <jason.stone0808@gmail.com>
2019-12-26 20:48:28 -0500

    Create App component and update index.js. Create SearchBox that filters robots

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "tachyons";
-import { robots } from "./robots";
-import CardList from "./CardList";
+import App from "./App";
 
-ReactDOM.render(<CardList robots={robots} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
