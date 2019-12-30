9faf7962a282e6650e855f82010696d7927d5314
jstone18 <jason.stone0808@gmail.com>
2019-12-30 16:15:45 -0500

    Updates

---
+++ precache-manifest.bbf0580c990a0bd5923bf3b9ef4c4d22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a8fefb69c7579773b3bd9f12bae9de",
+    "url": "/robofriends-app/index.html"
+  },
+  {
+    "revision": "7eb3beebc5013af825e8",
+    "url": "/robofriends-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "346106eae91b026e690b",
+    "url": "/robofriends-app/static/css/main.1575cf2d.chunk.css"
+  },
+  {
+    "revision": "7eb3beebc5013af825e8",
+    "url": "/robofriends-app/static/js/2.908ef5fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends-app/static/js/2.908ef5fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "346106eae91b026e690b",
+    "url": "/robofriends-app/static/js/main.e1eb58ac.chunk.js"
+  },
+  {
+    "revision": "8d6bb2cd24fed03d5da7",
+    "url": "/robofriends-app/static/js/runtime-main.d1653d3b.js"
+  }
+]);
